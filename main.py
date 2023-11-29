c234c317dfdd8f23dade42d72934a85b5b02954c
Воробьев Илья <ilya7050@gmail.com>
2023-11-29 14:07:21 +0700

    cli ui implemented added input validation

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 import datetime
 
 
+class NoSuchComman(Exception):
+    pass
+
+
 def from_str_to_datetime(date: str):
     "function to convert str DD.MM.YYYY to datetime"
     date = date.split('.')
@@ -18,7 +22,7 @@ def deadline_score(pass_date: str, deadline_date: str) -> int:
     if delta < 0:
         return 5
     elif delta // 7 == 0:
-        return 5
+        return 4
     elif delta // 7 == 1:
         return 4
     elif delta // 7 == 2:
@@ -44,12 +48,12 @@ def late_list(grades: dict, deadline_date: str) -> list[str]:
 
 def check_command(user_command: str) -> int:
     """"The function helps to manage errors"""
-    check = ['1', '2', '3', '4', '5', '6', 'help', 'quit']
+    check = ['1', '2', 'help', 'quit']
     quantity = 0
     for i in check:
         if i not in user_command:
             quantity += 1
-    if quantity == 8:
+    if quantity == 4:
         return 0
     return 1
 
@@ -57,13 +61,49 @@ def check_command(user_command: str) -> int:
 def main() -> None:
     print('Добро пожаловать в программу'
           'оценки задолжности студентов!')
-    user_deadline = input('Введите дедлайн(в формате DD.MM.YYYY): \n')
     print('1. Оценка студента исходя из даты сдачи работы \n'
-          '2. Список студентов просрочивших сдачу работы')
-    action = input('Выберите действие: ')
+          '2. Список студентов просрочивших сдачу работы \n'
+          'help - вывести список действий \n'
+          'quit - выйти')
     while True:
-        if 'quit' in action:
-            break
-        if 'help' in action:
-            print('1. Оценка студента исходя из даты сдачи работы \n'
-                  '2. Список студентов просрочивших сдачу работы')
+        try:
+            action = input('Выберите действие: ')
+            if 'quit' in action:
+                break
+            if 'help' in action:
+                print('1. Оценка студента исходя из даты сдачи работы \n'
+                      '2. Список студентов просрочивших сдачу работы')
+            if check_command(action) == 0:
+                raise NoSuchComman
+            if '1' in action:
+                user_deadline = input('Введите дедлайн(в формате DD.MM.YYYY): ')
+                from_str_to_datetime(user_deadline)
+                user_pass_date = input('Введите дату сдачи работы(в формате DD.MM.YYYY): ')
+                result = deadline_score(user_pass_date, user_deadline)
+                print(f'Оценка студента: {result}')
+            if '2' in action:
+                user_deadline = input('Введите дедлайн(в формате DD.MM.YYYY): ')
+                from_str_to_datetime(user_deadline)
+                user_dict = {}
+                print('Вводите фамилии студентов'
+                      'и дату сдачи работ'
+                      'через пробел \n'
+                      'Для окончания ввода используйте - end')
+                while True:
+                    user_pair = input('Фамилия дата: ')
+
+                    if user_pair == 'end':
+                        break
+                    else:
+                        user_pair = user_pair.split()
+                        user_dict[user_pair[0]] = user_pair[1]
+                print(f"Список студентов: "
+                      f"{''.join(late_list(user_dict, user_deadline))}")
+        except ValueError:
+            print('Используйте только формат DD.MM.YYYY')
+        except NoSuchComman:
+            print('Такой команды нет, попробуйте help')
+
+
+if __name__ == '__main__':
+    main()
