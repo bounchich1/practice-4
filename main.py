6d95888dbe58f8f1f188238cf4ed60076cf6548e
Воробьев Илья <ilya7050@gmail.com>
2023-11-29 13:20:41 +0700

    started implementing cli ui

--- main.py
+++ main.py
@@ -40,3 +40,30 @@ def late_list(grades: dict, deadline_date: str) -> list[str]:
         if int(str(pass_date - deadline_date)[:9].split()[0]) > 0:
             students.append(i)
     return students
+
+
+def check_command(user_command: str) -> int:
+    """"The function helps to manage errors"""
+    check = ['1', '2', '3', '4', '5', '6', 'help', 'quit']
+    quantity = 0
+    for i in check:
+        if i not in user_command:
+            quantity += 1
+    if quantity == 8:
+        return 0
+    return 1
+
+
+def main() -> None:
+    print('Добро пожаловать в программу'
+          'оценки задолжности студентов!')
+    user_deadline = input('Введите дедлайн(в формате DD.MM.YYYY): \n')
+    print('1. Оценка студента исходя из даты сдачи работы \n'
+          '2. Список студентов просрочивших сдачу работы')
+    action = input('Выберите действие: ')
+    while True:
+        if 'quit' in action:
+            break
+        if 'help' in action:
+            print('1. Оценка студента исходя из даты сдачи работы \n'
+                  '2. Список студентов просрочивших сдачу работы')
